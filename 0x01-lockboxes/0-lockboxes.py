888705c7306a7da80ef5e34bec99594518bde1d6
Sohaib Hegazy <sohaib.mohamed@hotmail.com>
2024-07-31 01:17:16 +0000

    modify

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,17 +5,17 @@ a Module with a method that determines if all the boxes can be opened
 
 
 def canUnlockAll(boxes):
-  '''
-  a method that determines if all the boxes can be opened
-  '''
+    '''
+    a method that determines if all the boxes can be opened
+    '''
 
-  if not boxes or type(boxes) is not list:
+    if not boxes or type(boxes) is not list:
         return False
-  
-  unlocked = [0]
 
-  for box in unlocked:
-      for key in boxes[box]:
-          if key not in unlocked and key < len(boxes):
-              unlocked.append(key)
-  return len(unlocked) == len(boxes)
+    unlocked = [0]
+
+    for box in unlocked:
+        for key in boxes[box]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    return len(unlocked) == len(boxes)
