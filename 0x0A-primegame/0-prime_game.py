726e7edc65bedd76eefb8e2704bb7543bf00ed60
SohaibHegazy <sohaib.mohamed@hotmail.com>
2024-10-01 05:36:53 +0000

    modify

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,48 +3,32 @@
 The Prime Game
 '''
 
-
+    
 def isWinner(x, nums):
     '''
     The function to determine the winner
     '''
-    def sieve_of_eratosthenes(n):
-        '''
-        Helper function to find prime numbers up to a given number n
-        '''
-        primes = [True] * (n + 1)
-        p = 2
-        while (p * p <= n):
-            if primes[p] == True:
-                for i in range(p * p, n + 1, p):
-                    primes[i] = False
-            p += 1
-        prime_list = []
-        for p in range(2, n + 1):
-            if primes[p]:
-                prime_list.append(p)
-        return prime_list
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if n == 1:
-            ben_wins += 1
-            continue
-        
-        primes = sieve_of_eratosthenes(n)
-        prime_turns = len(primes)
-        
-        if prime_turns % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if not nums or x < 1:
         return None
+    n = max(nums)
+    nums.sort()
+    m = [False for i in range(n + 1)]
+    for i in range(2, int(n ** 0.5) + 1):
+        if not m[i]:
+            for j in range(i*i, n + 1, i):
+                m[j] = True
+    m[0] = m[1] = True
+    c = 0
+    for i in range(len(m)):
+        if not m[i]:
+            c += 1
+        m[i] = c
+    p1 = 0
+    for n in nums:
+        p1 += m[n] % 2 == 1
+    if p1 * 2 == len(nums):
+        return None
+    if p1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
 
